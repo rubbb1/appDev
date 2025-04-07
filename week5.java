a3f1634d2338848672a896096416558a2ec4719c
Rubrobb Ondoy <rondoy@paterostechnologicalcollege.edu.ph>
2025-04-07 13:59:01 +0800

    Finished Activity 5

---
+++ week5.java
@@ -0,0 +1,8 @@
+public class week5 {
+    public static void main(String[] args) {
+        System.out.println("Collaborative Java Project\n");
+
+        // Call each student's method
+        Ondoy.display(); // Add your method call here
+    }
+}
