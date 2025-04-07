5be2dbf92f1f295a6933defa3c745f47a4db7c8e
Rubrobb Ondoy <rondoy@paterostechnologicalcollege.edu.ph>
2025-04-07 13:51:02 +0800

    Finished Activity 3

---
+++ Activity3.java
@@ -0,0 +1,10 @@
+public class Activity3 {
+    public static void main(String[] args) {
+        System.out.println("List of Students:");
+   
+        // Collaborators: Add your names below using the format:
+        // System.out.println("Your Full Name - SECTION");
+
+        System.out.println("Rubrobb Ondoy - BSIT 2I");
+    }
+}
